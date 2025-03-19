81ac3ce02a9d9e89cd2a628ffde79722284e68f0
Zootyr <lauriloiskekoski@gmail.com>
2025-03-19 14:00:03 +0200

    Update App_unicafe.jsx 1.8

--- part_1/App_unicafe.jsx
+++ part_1/App_unicafe.jsx
@@ -1,13 +1,34 @@
 import { useState } from 'react';
 
+const Statistics = (props) => {
+  const good = props.stats[0]
+  const neutral = props.stats[1]
+  const bad = props.stats[2]
+  const total = good + bad + neutral;
+  const average = (good - bad) / total;
+  const positive = (good / total) * 100;
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <p> Good {good} </p>
+      <p> Neutral {neutral} </p>
+      <p> Bad {bad} </p>
+      <p> All {total} </p>
+      <p> Average {average} </p>
+      <p> Positive {positive} % </p>
+    </div>
+  );
+}
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const total = good + bad + neutral;
-  const average = (good - bad) / total;
-  const positive = (good / total) * 100;
+
+  const stats = [good, neutral, bad]
+
+
 
   return (
     <div>
@@ -17,13 +38,8 @@ const App = () => {
         <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
         <button onClick={() => setBad(bad + 1)}>bad</button>
       </div>
-      <h1> Statistics </h1>
-      <p> Good {good}</p>
-      <p> Neutral {neutral}</p>
-      <p> Bad {bad}</p>
-      <p> All {total} </p>
-      <p> Average {average} </p>
-      <p> Positive {positive} % </p>
+      <Statistics stats = {stats}/>
+
     </div>
   );
 };
