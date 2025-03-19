566b1b89861682e374da4ac9476ca36baea88ad8
Zootyr <lauriloiskekoski@gmail.com>
2025-03-19 13:31:30 +0200

    Update App_unicafe.jsx to 1.6

--- part_1/App_unicafe.jsx
+++ part_1/App_unicafe.jsx
@@ -1,1 +1,24 @@
+import { useState } from 'react'
 
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1> Give feedback </h1>
+      <div>
+        <button onClick={() => setGood(good + 1)}>good</button>
+        <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+        <button onClick={() => setBad(bad + 1)}>bad</button>
+      </div>
+      <h1> Statistics </h1>
+      <p> Good {good}</p>
+      <p> Neutral {neutral}</p>
+      <p> Bad {bad}</p>
+    </div>
+  )
+}
+export default App
